f2e8e7333ec5afeaa7139a4152134a07736f856c
Bartosz Skotarek <bzskotarek@gmail.com>
2019-09-27 17:03:48 +0200

    [added] Communication test [refactored] Tests - new structure, common test method, tests are focused on business logic

--- report-jasper-reports/src/test/kotlin/pl/beone/promena/transformer/report/jasperreports/JasperReportsReportTransformerCommunicationTest.kt
+++ report-jasper-reports/src/test/kotlin/pl/beone/promena/transformer/report/jasperreports/JasperReportsReportTransformerCommunicationTest.kt
@@ -13,17 +13,12 @@ import java.sql.Timestamp
 import java.util.*
 
 @ExtendWith(DockerExtension::class)
-class JasperReportsReportTransformerTest {
+class JasperReportsReportTransformerCommunicationTest {
 
     companion object {
-        private const val assertEmptyElement =
-            "null null null null null null null null null null null null"
         private const val assertElement =
             "value true 1.1 2.2 3 4 5 6 1/1/70, 12:36 AM 1/1/70, 12:20 AM 1/3/70, 11:12 AM 1/2/70, 10:12 AM"
-        private const val assertElement2 =
-            "fieldValue false 11.1 12.2 13 14 15 16 12/17/93, 12:00 PM 5/3/91, 10:00 AM 10/10/91, 11:00 AM 1/1/00, 12:00 AM"
 
-        private val emptyParameters = emptyJasperReportsParameters()
         private val parameters = emptyJasperReportsParameters() +
                 ("string" to "value") +
                 ("boolean" to true) +
@@ -38,7 +33,6 @@ class JasperReportsReportTransformerTest {
                 ("sqlTime" to Time(213123321)) +
                 ("sqlTimestamp" to Timestamp(123123213))
 
-        private val emptyRecord = emptyJasperReportsRecord()
         private val record = emptyJasperReportsRecord() +
                 ("string" to "value") +
                 ("boolean" to true) +
@@ -52,63 +46,23 @@ class JasperReportsReportTransformerTest {
                 ("sqlDate" to java.sql.Date(1213213)) +
                 ("sqlTime" to Time(213123321)) +
                 ("sqlTimestamp" to Timestamp(123123213))
-        private val record2 = emptyJasperReportsRecord() +
-                ("string" to "fieldValue") +
-                ("boolean" to false) +
-                ("double" to 11.1) +
-                ("float" to 12.2f) +
-                ("integer" to 13) +
-                ("long" to 14L) +
-                ("short" to 15.toShort()) +
-                ("bigDecimal" to BigDecimal("16")) +
-                ("date" to Date(756129600000)) +
-                ("sqlDate" to java.sql.Date(-5638082400000)) +
-                ("sqlTime" to Time(687092400000)) +
-                ("sqlTimestamp" to Timestamp(946684800000))
     }
 
     @Test
-    fun transform_allSet() {
+    fun transform_memoryTest() {
         memoryTest(
-            jasperReportsReportParameters(records = record + record2, parameters = parameters),
+            jasperReportsReportParameters(records = listOf(record), parameters = parameters),
             assertElement,
-            listOf(assertElement, assertElement2)
+            listOf(assertElement)
         )
     }
 
     @Test
-    fun transform_emptyFirstRecord() {
-        memoryTest(
-            jasperReportsReportParameters(records = emptyRecord + record2, parameters = parameters),
+    fun transform_fileTest() {
+        fileTest(
+            jasperReportsReportParameters(records = listOf(record), parameters = parameters),
             assertElement,
-            listOf(assertEmptyElement, assertElement2)
-        )
-    }
-
-    @Test
-    fun transform_emptyParameters() {
-        memoryTest(
-            jasperReportsReportParameters(records = listOf(record2), parameters = emptyParameters),
-            assertEmptyElement,
-            listOf(assertElement2)
-        )
-    }
-
-    @Test
-    fun transform_noParameters() {
-        memoryTest(
-            jasperReportsReportParameters(records = listOf(record2)),
-            assertEmptyElement,
-            listOf(assertElement2)
-        )
-    }
-
-    @Test
-    fun transform_emptyParametersAndEmptyRecord() {
-        memoryTest(
-            jasperReportsReportParameters(records = listOf(emptyRecord), parameters = emptyParameters),
-            assertEmptyElement,
-            listOf(assertEmptyElement)
+            listOf(assertElement)
         )
     }
 }
---
+++ report-jasper-reports/src/test/kotlin/pl/beone/promena/transformer/report/jasperreports/util/ResourceUtils.kt
@@ -0,0 +1,9 @@
+package pl.beone.promena.transformer.report.jasperreports.util
+
+import java.io.InputStream
+
+internal fun getResourceAsBytes(path: String): ByteArray =
+    getResourceAsInputStream(path).readAllBytes()
+
+internal fun getResourceAsInputStream(path: String): InputStream =
+    object {}::class.java.getResourceAsStream(path) ?: throw IllegalArgumentException("There is no <$path> resource")
